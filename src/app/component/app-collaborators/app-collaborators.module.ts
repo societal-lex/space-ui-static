3646d2043b011bf17d86b6e24b092ad440e0c908
kamaleeswary <kamaleeswary@gmail.com>
2020-06-26 13:33:10 +0530

    bugs-fixed responsive ui,fonts,logo

--- src/app/component/app-collaborators/app-collaborators.module.ts
+++ src/app/component/app-collaborators/app-collaborators.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { AppCollaboratorsComponent } from './app-collaborators.component'
 
 @NgModule({
-  declarations: [AppCollaboratorsComponent],
+  declarations: [],
   imports: [
     CommonModule,
   ],
-  exports: [AppCollaboratorsComponent],
 })
 export class AppCollaboratorsModule { }
