0030d5d3b1538f157addddc85e3c0e37f178d0d1
Tanya Chauhan <Tanya.Chauhan@stackroute.com>
2020-06-30 19:11:00 +0530

    activities,featured bugs resolved

---
+++ src/app/routes/public/dataprivacy/dataprivacy.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core'
+import { ConfigurationsService, NsPage } from '@ws-widget/utils'
+
+@Component({
+  selector: 'ws-dataprivacy',
+  templateUrl: './dataprivacy.component.html',
+  styleUrls: ['./dataprivacy.component.scss'],
+})
+export class DataprivacyComponent implements OnInit {
+  pageNavbar: Partial<NsPage.INavBackground> = this.configSvc.pageNavBar
+
+  constructor(private configSvc: ConfigurationsService) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/routes/public/termsofuse/termsofuse.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core'
+import { ConfigurationsService, NsPage } from '@ws-widget/utils'
+
+@Component({
+  selector: 'ws-termsofuse',
+  templateUrl: './termsofuse.component.html',
+  styleUrls: ['./termsofuse.component.scss'],
+})
+export class TermsofuseComponent implements OnInit {
+  pageNavbar: Partial<NsPage.INavBackground> = this.configSvc.pageNavBar
+
+  constructor(private configSvc: ConfigurationsService) { }
+
+  ngOnInit() {
+  }
+
+}
