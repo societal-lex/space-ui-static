3fcbdcd3f15e16fb8c28eef0745d44c571943817
Tanya Chauhan <Tanya.Chauhan@stackroute.com>
2020-07-09 00:16:46 +0530

    added keyboard event in blog search , added policy page with updated font, added some details in overview

--- project/ws/app/src/lib/routes/social/routes/blogs/recent-blogs/components/recent-blog.component.ts
+++ project/ws/app/src/lib/routes/social/routes/blogs/recent-blogs/components/recent-blog.component.ts
@@ -106,7 +106,7 @@ export class RecentBlogComponent implements OnInit {
   triggerSearch() {
 
     this.router.navigate(['/app/social/socialSearch/view-search'],
-      { queryParams: { search: this.queryEntered } })
+                         { queryParams: { search: this.queryEntered } })
 
   }
 }
