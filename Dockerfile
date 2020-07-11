3dea2d86b83e7f9d7b09a416fb47642de384c166
ArjunKhetia-NIIT <arjunj@ilimi.in>
2020-07-11 15:47:10 +0530

    Modification done

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM node:10
 
 WORKDIR /app
-COPY . .
+COPY dist .
 
 #RUN npm i yarn
 #RUN yarn global add @angular/cli@latest
 
-RUN yarn && yarn add moment && npm run build --prod --build-optimizer
+# RUN npm run build
 #RUN ng build --prod --outputPath=dist/www/en --baseHref=/ --i18nLocale=en --verbose=true
-RUN npm run compress:brotli
+# RUN npm run compress:brotli
 #RUN npm run compress:gzip
 
 WORKDIR /app/dist
--- library/ws-widget/collection/src/lib/btn-catalog/btn-catalog.component.ts
+++ library/ws-widget/collection/src/lib/btn-catalog/btn-catalog.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core'
 import { TreeCatalogService } from '../tree-catalog/tree-catalog.service'
-import { TFetchStatus } from '@ws-widget/utils'
+import { TFetchStatus, ConfigurationsService } from '@ws-widget/utils'
+import { FormControl } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { ISearchAutoComplete, ISearchQuery } from '../../../../../../project/ws/app/src/lib/routes/search/models/search.model'
+import { SearchServService } from '../../../../../../project/ws/app/src/lib/routes/search/services/search-serv.service'
 import { NSSearch } from '../_services/widget-search.model'
 import { NsWidgetResolver, WidgetBaseComponent } from '@ws-widget/resolver'
 
@@ -15,9 +19,32 @@ export class BtnCatalogComponent extends WidgetBaseComponent
   @Input() widgetData!: any
   catalogItems: NSSearch.IFilterUnitContent[] | null = null
   catalogFetchStatus: TFetchStatus = 'none'
+  query: FormControl = new FormControl('')
+  searchQuery: ISearchQuery = {
+    l: this.getActivateLocale(),
+    q: '',
+  }
+  autoCompleteResults: ISearchAutoComplete[] = []
 
-  constructor(private catalogSvc: TreeCatalogService) {
+  constructor(private catalogSvc: TreeCatalogService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private searchSvc: SearchServService,
+    private configSvc: ConfigurationsService) {
     super()
+  }
+
+  getActivateLocale(): string {
+    const locale = (this.configSvc.activeLocale && this.configSvc.activeLocale.locals[0]) || 'en'
+    return this.searchSvc.getLanguageSearchIndex(locale)
+  }
+
+  getAutoCompleteResults(): void {
+    this.searchSvc.searchAutoComplete(this.searchQuery).then((results: ISearchAutoComplete[]) => {
+      this.autoCompleteResults = results
+    }).catch(() => {
+
+    })
   }
 
   getCatalog() {
