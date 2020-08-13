aa8e0e6c27c6cee3c10638635b1dc02e1ed4a178
Tanya Chauhan <Tanya.Chauhan@stackroute.com>
2020-08-13 16:50:57 +0530

    edited the size of star and profile

--- src/app/routes/public/register-user/components/userdeatils/userdeatils.component.ts
+++ src/app/routes/public/register-user/components/userdeatils/userdeatils.component.ts
@@ -29,11 +29,11 @@ export class UserdeatilsComponent implements OnInit {
     if (certiType === 1) {
       this.router.navigate([`/public/guides/certificates/${1}/${defaultUserID ? defaultUserID :
         this.currentUserDetails.source_id}`],
-        { state: this.currentUserDetails, relativeTo: this.route })
+                           { state: this.currentUserDetails, relativeTo: this.route })
     } else if (certiType === 2) {
       this.router.navigate([`/public/guides/certificates/${2}/${defaultUserID ? defaultUserID :
         this.currentUserDetails.source_id}`],
-        { state: this.currentUserDetails, relativeTo: this.route })
+                           { state: this.currentUserDetails, relativeTo: this.route })
     }
   }
 
