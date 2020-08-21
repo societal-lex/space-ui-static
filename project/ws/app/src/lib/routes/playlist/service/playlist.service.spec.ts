74d5881ea6e7c169fb7d38de6e7785fe26e39e65
rishabh kalra <rishabh.kalra@niit.com>
2020-08-21 08:26:49 +0530

    lint fix

--- project/ws/app/src/lib/routes/playlist/service/playlist.service.spec.ts
+++ project/ws/app/src/lib/routes/playlist/service/playlist.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { PlaylistService } from './playlist.service';
+import { PlaylistService } from './playlist.service'
 
 describe('PlaylistService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({}))
 
   it('should be created', () => {
-    const service: PlaylistService = TestBed.get(PlaylistService);
-    expect(service).toBeTruthy();
-  });
-});
+    const service: PlaylistService = TestBed.get(PlaylistService)
+    expect(service).toBeTruthy()
+  })
+})
