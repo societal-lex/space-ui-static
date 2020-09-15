c2d61d62397a3c06073facc47ac83fb9384e49c0
Tanya Chauhan <Tanya.Chauhan@stackroute.com>
2020-09-15 15:54:23 +0530

    build error

--- src/app/component/about-collaborator/about-collaborator.module.ts
+++ src/app/component/about-collaborator/about-collaborator.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { AboutCollaboratorComponent } from './about-collaborator.component'
+// import { AboutCollaboratorComponent } from './about-collaborator.component'
 import {
   MatToolbarModule,
   MatDividerModule,
